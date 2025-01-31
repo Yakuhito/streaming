72c983adcb41c0bc2c52518e458397825195a348
Yak <y@kuhi.to>
2025-01-31 09:42:21 -0300

    layer update

--- src/layer.rs
+++ src/layer.rs
@@ -5,31 +5,33 @@ use clvm_utils::{CurriedProgram, ToTreeHash, TreeHash};
 use clvmr::{Allocator, NodePtr};
 use hex_literal::hex;
 
-pub const STREAM_PUZZLE: [u8; 484] =
-    hex!("ff02ffff01ff02ffff03ffff09ff81ffffff05ffff14ffff12ff5fffff11ff81bfff2f8080ffff11ff0bff2f80808080ffff01ff04ffff04ff08ffff04ff5fff808080ffff04ffff04ff14ffff04ff81bfff808080ffff04ffff04ff1cffff04ff05ffff04ff81ffffff04ffff04ff05ff8080ff8080808080ffff04ffff04ff1cffff04ffff0bff5effff0bff16ffff0bff16ff6eff1780ffff0bff16ffff0bff7effff0bff16ffff0bff16ff6effff0bffff0101ff178080ffff0bff16ffff0bff7effff0bff16ffff0bff16ff6effff0bffff0101ff81bf8080ffff0bff16ff6eff4e808080ff4e808080ff4e808080ffff04ffff11ff5fff81ff80ffff04ffff04ffff0bffff0173ff0580ff8080ff8080808080ffff04ffff04ff0affff04ffff0117ffff04ff81bfffff04ff05ff8080808080ff808080808080ffff01ff088080ff0180ffff04ffff01ffff49ff5133ff43ff02ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff018080");
+pub const STREAM_PUZZLE: [u8; 548] =
+    hex!("ff02ffff01ff02ffff03ffff09ff8202ffffff05ffff14ffff12ff81bfffff11ff82017fff5f8080ffff11ff17ff5f80808080ffff01ff04ffff04ff14ffff04ff81bfff808080ffff04ffff03ff8203ffffff04ff08ffff04ffff10ff82017fffff010180ff808080ffff04ff1cffff04ff82017fff80808080ffff04ffff04ff12ffff04ff05ffff04ff8202ffffff04ffff04ff05ff8080ff8080808080ffff04ffff04ff12ffff04ffff03ff8203ffff0bffff0bff5effff0bff16ffff0bff16ff6eff2f80ffff0bff16ffff0bff7effff0bff16ffff0bff16ff6effff0bffff0101ff2f8080ffff0bff16ffff0bff7effff0bff16ffff0bff16ff6effff0bffff0101ff82017f8080ffff0bff16ff6eff4e808080ff4e808080ff4e80808080ffff04ffff11ff81bfff8202ff80ffff04ffff04ffff0bffff0173ff0580ff8080ff8080808080ffff04ffff04ff1affff04ffff0117ffff04ff82017fffff04ffff03ff8203ffff0bff0580ff8080808080ff808080808080ffff01ff088080ff0180ffff04ffff01ffff55ff4951ffff3343ff02ffffa04bf5122f344554c53bde2ebb8cd2b7e3d1600ad631c385a5d7cce23c7785459aa09dcf97a184f32623d11a73124ceb99a5709b083721e878a16d78f596718ba7b2ffa102a12871fee210fb8619291eaea194581cbd2531e4b23759d225f6806923f63222a102a8d5dd63fba471ebcb1f3e8f7c1e1879b7152a6e7298a91ce119a63400ade7c5ff018080");
 pub const STREAM_PUZZLE_HASH: TreeHash = TreeHash::new(hex!(
-    "fb1d2cd040e46ba16f134804d319cfbafb7c736a3c57968e10059b9a5d868773"
+    "68bdae089c871b1be27f6bacc9b7b32fc6677ff20cd275af26b62b1c5d07bb54"
 ));
 
 #[derive(ToClvm, FromClvm, Debug, Clone, Copy, PartialEq, Eq)]
 #[clvm(curry)]
 pub struct StreamPuzzle1stCurryArgs {
     pub recipient: Bytes32,
+    pub clawback_ph: Bytes32,
     pub end_time: u64,
 }
 
 impl StreamPuzzle1stCurryArgs {
-    pub fn new(recipient: Bytes32, end_time: u64) -> Self {
+    pub fn new(recipient: Bytes32, clawback_ph: Bytes32, end_time: u64) -> Self {
         Self {
             recipient,
+            clawback_ph,
             end_time,
         }
     }
 
-    pub fn curry_tree_hash(recipient: Bytes32, end_time: u64) -> TreeHash {
+    pub fn curry_tree_hash(recipient: Bytes32, clawback_ph: Bytes32, end_time: u64) -> TreeHash {
         CurriedProgram {
             program: STREAM_PUZZLE_HASH,
-            args: StreamPuzzle1stCurryArgs::new(recipient, end_time),
+            args: StreamPuzzle1stCurryArgs::new(recipient, clawback_ph, end_time),
         }
         .tree_hash()
     }
@@ -50,8 +52,13 @@ impl StreamPuzzle2ndCurryArgs {
         }
     }
 
-    pub fn curry_tree_hash(recipient: Bytes32, end_time: u64, last_payment_time: u64) -> TreeHash {
-        let self_hash = StreamPuzzle1stCurryArgs::curry_tree_hash(recipient, end_time);
+    pub fn curry_tree_hash(
+        recipient: Bytes32,
+        clawback_ph: Bytes32,
+        end_time: u64,
+        last_payment_time: u64,
+    ) -> TreeHash {
+        let self_hash = StreamPuzzle1stCurryArgs::curry_tree_hash(recipient, clawback_ph, end_time);
         CurriedProgram {
             program: self_hash,
             args: StreamPuzzle2ndCurryArgs::new(self_hash.into(), last_payment_time),
@@ -65,8 +72,9 @@ impl StreamPuzzle2ndCurryArgs {
 pub struct StreamPuzzleSolution {
     pub my_amount: u64,
     pub payment_time: u64,
-    #[clvm(rest)]
     pub to_pay: u64,
+    #[clvm(rest)]
+    pub clawback: bool,
 }
 
 impl Mod for StreamPuzzle1stCurryArgs {
@@ -77,14 +85,21 @@ impl Mod for StreamPuzzle1stCurryArgs {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct StreamLayer {
     pub recipient: Bytes32,
+    pub clawback_ph: Bytes32,
     pub end_time: u64,
     pub last_payment_time: u64,
 }
 
 impl StreamLayer {
-    pub fn new(recipient: Bytes32, end_time: u64, last_payment_time: u64) -> Self {
+    pub fn new(
+        recipient: Bytes32,
+        clawback_ph: Bytes32,
+        end_time: u64,
+        last_payment_time: u64,
+    ) -> Self {
         Self {
             recipient,
+            clawback_ph,
             end_time,
             last_payment_time,
         }
@@ -93,6 +108,7 @@ impl StreamLayer {
     pub fn puzzle_hash(&self) -> TreeHash {
         StreamPuzzle2ndCurryArgs::curry_tree_hash(
             self.recipient,
+            self.clawback_ph,
             self.end_time,
             self.last_payment_time,
         )
@@ -135,6 +151,7 @@ impl Layer for StreamLayer {
 
         Ok(Some(Self {
             recipient: args1.recipient,
+            clawback_ph: args1.clawback_ph,
             end_time: args1.end_time,
             last_payment_time: args2.last_payment_time,
         }))
@@ -148,9 +165,16 @@ impl Layer for StreamLayer {
     }
 
     fn construct_puzzle(&self, ctx: &mut SpendContext) -> Result<NodePtr, DriverError> {
-        let puzzle_1st_curry =
-            ctx.curry(StreamPuzzle1stCurryArgs::new(self.recipient, self.end_time))?;
-        let self_hash = StreamPuzzle1stCurryArgs::curry_tree_hash(self.recipient, self.end_time);
+        let puzzle_1st_curry = ctx.curry(StreamPuzzle1stCurryArgs::new(
+            self.recipient,
+            self.clawback_ph,
+            self.end_time,
+        ))?;
+        let self_hash = StreamPuzzle1stCurryArgs::curry_tree_hash(
+            self.recipient,
+            self.clawback_ph,
+            self.end_time,
+        );
 
         CurriedProgram {
             program: puzzle_1st_curry,
